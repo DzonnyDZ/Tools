c6232f38c57ebf5c4cace0e5bc4727b1b8d76ec1
Dzonny_cp <SND\Dzonny_cp>
2007-01-09 07:50:40 +0800

    null

--- Tools/Windows/Forms/LinkLabel.vb
+++ Tools/Windows/Forms/LinkLabel.vb
@@ -669,6 +669,12 @@ Namespace Windows.Forms
                     Return New CollectionForm(Me)
                 End If
             End Function
+            ''' <summary>Edits the value of the specified object using the specified service provider and context.</summary>
+            ''' <param name="context">An <see cref="System.ComponentModel.ITypeDescriptorContext"/> that can be used to gain additional context information.</param>
+            ''' <param name="value">The object to edit the value of.</param>
+            ''' <param name="provider">A service provider object through which editing services can be obtained.</param>
+            ''' <returns>The new value of the object. If the value of the object has not changed, this should return the same object it was passed.</returns>
+            ''' <exception cref="System.ComponentModel.Design.CheckoutException">An attempt to check out a file that is checked into a source code management program did not succeed</exception>
             Public Overrides Function EditValue(ByVal context As System.ComponentModel.ITypeDescriptorContext, ByVal provider As System.IServiceProvider, ByVal value As Object) As Object
                 EditValue = MyBase.EditValue(context, provider, value)
                 If context IsNot Nothing AndAlso context.Instance IsNot Nothing AndAlso provider IsNot Nothing Then
