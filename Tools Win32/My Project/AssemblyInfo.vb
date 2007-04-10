e76faf685e1805e867b9d7eca81f0c4d3309c004
Dzonny_cp <SND\Dzonny_cp>
2007-04-11 04:53:40 +0800

    Tools Win32

---
+++ Tools Win32/My Project/AssemblyInfo.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Tools Win32")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("ĐTools team")> 
+<Assembly: AssemblyProduct("ĐTools")> 
+<Assembly: AssemblyCopyright("Copyright © ĐTools team 2007")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b4f21e90-48be-4ed6-ac00-00dc84a8fad8")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: CLSCompliant(True)> 
+<Assembly: DoNotApplyAuthorAndVersionAttributes()> 
