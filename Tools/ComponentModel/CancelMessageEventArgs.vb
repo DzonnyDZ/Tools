b0883d6d29f26f02dce093a3941d77a35ca2c267
Dzonny_cp <SND\Dzonny_cp>
2007-05-24 06:41:45 +0800

    null

--- Tools/ComponentModel/CancelMessageEventArgs.vb
+++ Tools/ComponentModel/CancelMessageEventArgs.vb
@@ -1,8 +1,9 @@
 Namespace ComponentModel
-#If Config <= Nightly Then 'Stage: Nightly
+#If Config <= Beta Then 'Stage: Beta
+    ''' <summary><see cref="CancelEventArgs"/> with message witn reason for cancellation</summary>
     <Author("Ðonny", "dzonny@dzonny.cz", "http://dzonny.cz")> _
     <Version(1, 0, GetType(CancelMessageEventArgs), LastChMMDDYYYY:="05/20/2007")> _
-    Public Class CancelMessageEventArgs : Inherits CancelEventArgs 'ASAP:Wiki,forum
+    Public Class CancelMessageEventArgs : Inherits CancelEventArgs
         ''' <summary>Contains value of the <see cref="CancelMessage"/> property</summary>
         Private _CancelMessage As String
         ''' <summary>Human readabele cancel reason reported to source of the event"</summary>
