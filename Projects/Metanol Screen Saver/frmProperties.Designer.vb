e6db1ad3d577267df2ca7ee5c05cd14584aa71b2
Dzonny_cp <SND\Dzonny_cp>
2007-12-01 21:49:52 +0800

    null

---
+++ Projects/Metanol Screen Saver/frmProperties.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmProperties
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmProperties))
+        Me.tlpMain = New System.Windows.Forms.TableLayoutPanel
+        Me.fraIPTC = New System.Windows.Forms.GroupBox
+        Me.lstIPTC = New System.Windows.Forms.ListBox
+        Me.fraExif = New System.Windows.Forms.GroupBox
+        Me.lstExif = New System.Windows.Forms.ListBox
+        Me.fraOther = New System.Windows.Forms.GroupBox
+        Me.lstOther = New System.Windows.Forms.ListBox
+        Me.txtInfo = New System.Windows.Forms.TextBox
+        Me.fraInfo = New System.Windows.Forms.GroupBox
+        Me.txtHelp = New System.Windows.Forms.TextBox
+        Me.tlpMain.SuspendLayout()
+        Me.fraIPTC.SuspendLayout()
+        Me.fraExif.SuspendLayout()
+        Me.fraOther.SuspendLayout()
+        Me.fraInfo.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tlpMain
+        '
+        Me.tlpMain.ColumnCount = 3
+        Me.tlpMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.tlpMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.tlpMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.tlpMain.Controls.Add(Me.fraIPTC, 0, 0)
+        Me.tlpMain.Controls.Add(Me.fraExif, 1, 0)
+        Me.tlpMain.Controls.Add(Me.fraOther, 2, 0)
+        Me.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tlpMain.Location = New System.Drawing.Point(0, 0)
+        Me.tlpMain.Name = "tlpMain"
+        Me.tlpMain.RowCount = 1
+        Me.tlpMain.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tlpMain.Size = New System.Drawing.Size(429, 137)
+        Me.tlpMain.TabIndex = 0
+        '
+        'fraIPTC
+        '
+        Me.fraIPTC.Controls.Add(Me.lstIPTC)
+        Me.fraIPTC.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.fraIPTC.Location = New System.Drawing.Point(0, 0)
+        Me.fraIPTC.Margin = New System.Windows.Forms.Padding(0)
+        Me.fraIPTC.Name = "fraIPTC"
+        Me.fraIPTC.Size = New System.Drawing.Size(143, 137)
+        Me.fraIPTC.TabIndex = 0
+        Me.fraIPTC.TabStop = False
+        Me.fraIPTC.Text = "IPTC"
+        '
+        'lstIPTC
+        '
+        Me.lstIPTC.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lstIPTC.FormattingEnabled = True
+        Me.lstIPTC.Location = New System.Drawing.Point(3, 16)
+        Me.lstIPTC.Name = "lstIPTC"
+        Me.lstIPTC.Size = New System.Drawing.Size(137, 108)
+        Me.lstIPTC.TabIndex = 0
+        '
+        'fraExif
+        '
+        Me.fraExif.Controls.Add(Me.lstExif)
+        Me.fraExif.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.fraExif.Location = New System.Drawing.Point(143, 0)
+        Me.fraExif.Margin = New System.Windows.Forms.Padding(0)
+        Me.fraExif.Name = "fraExif"
+        Me.fraExif.Size = New System.Drawing.Size(143, 137)
+        Me.fraExif.TabIndex = 1
+        Me.fraExif.TabStop = False
+        Me.fraExif.Text = "Exif"
+        '
+        'lstExif
+        '
+        Me.lstExif.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lstExif.FormattingEnabled = True
+        Me.lstExif.Location = New System.Drawing.Point(3, 16)
+        Me.lstExif.Name = "lstExif"
+        Me.lstExif.Size = New System.Drawing.Size(137, 108)
+        Me.lstExif.TabIndex = 0
+        '
+        'fraOther
+        '
+        Me.fraOther.Controls.Add(Me.lstOther)
+        Me.fraOther.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.fraOther.Location = New System.Drawing.Point(286, 0)
+        Me.fraOther.Margin = New System.Windows.Forms.Padding(0)
+        Me.fraOther.Name = "fraOther"
+        Me.fraOther.Size = New System.Drawing.Size(143, 137)
+        Me.fraOther.TabIndex = 2
+        Me.fraOther.TabStop = False
+        Me.fraOther.Text = "Other"
+        '
+        'lstOther
+        '
+        Me.lstOther.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lstOther.FormattingEnabled = True
+        Me.lstOther.Location = New System.Drawing.Point(3, 16)
+        Me.lstOther.Name = "lstOther"
+        Me.lstOther.Size = New System.Drawing.Size(137, 108)
+        Me.lstOther.TabIndex = 0
+        '
+        'txtInfo
+        '
+        Me.txtInfo.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtInfo.Location = New System.Drawing.Point(3, 16)
+        Me.txtInfo.MaxLength = 0
+        Me.txtInfo.Multiline = True
+        Me.txtInfo.Name = "txtInfo"
+        Me.txtInfo.ReadOnly = True
+        Me.txtInfo.Size = New System.Drawing.Size(423, 50)
+        Me.txtInfo.TabIndex = 1
+        Me.txtInfo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'fraInfo
+        '
+        Me.fraInfo.Controls.Add(Me.txtInfo)
+        Me.fraInfo.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.fraInfo.Location = New System.Drawing.Point(0, 137)
+        Me.fraInfo.Name = "fraInfo"
+        Me.fraInfo.Size = New System.Drawing.Size(429, 69)
+        Me.fraInfo.TabIndex = 2
+        Me.fraInfo.TabStop = False
+        Me.fraInfo.Text = "GroupBox1"
+        '
+        'txtHelp
+        '
+        Me.txtHelp.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.txtHelp.Location = New System.Drawing.Point(0, 206)
+        Me.txtHelp.MaxLength = 0
+        Me.txtHelp.Multiline = True
+        Me.txtHelp.Name = "txtHelp"
+        Me.txtHelp.ReadOnly = True
+        Me.txtHelp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtHelp.Size = New System.Drawing.Size(429, 82)
+        Me.txtHelp.TabIndex = 3
+        Me.txtHelp.Text = resources.GetString("txtHelp.Text")
+        '
+        'frmProperties
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(429, 288)
+        Me.Controls.Add(Me.tlpMain)
+        Me.Controls.Add(Me.fraInfo)
+        Me.Controls.Add(Me.txtHelp)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmProperties"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Info template designer"
+        Me.tlpMain.ResumeLayout(False)
+        Me.fraIPTC.ResumeLayout(False)
+        Me.fraExif.ResumeLayout(False)
+        Me.fraOther.ResumeLayout(False)
+        Me.fraInfo.ResumeLayout(False)
+        Me.fraInfo.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents tlpMain As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents fraIPTC As System.Windows.Forms.GroupBox
+    Friend WithEvents lstIPTC As System.Windows.Forms.ListBox
+    Friend WithEvents fraExif As System.Windows.Forms.GroupBox
+    Friend WithEvents lstExif As System.Windows.Forms.ListBox
+    Friend WithEvents fraOther As System.Windows.Forms.GroupBox
+    Friend WithEvents lstOther As System.Windows.Forms.ListBox
+    Friend WithEvents txtInfo As System.Windows.Forms.TextBox
+    Friend WithEvents fraInfo As System.Windows.Forms.GroupBox
+    Friend WithEvents txtHelp As System.Windows.Forms.TextBox
+End Class
---
+++ Projects/Metanol Screen Saver/frmProperties.vb
@@ -0,0 +1,105 @@
+﻿Imports System.Reflection, Tools.TypeTools
+Imports System.ComponentModel
+
+Public Class frmProperties
+    Private Shared ExifProperties As IEnumerable(Of PropertyInfoDisplay)
+    Private Shared IPTCProperties As IEnumerable(Of PropertyInfoDisplay)
+    Private Shared SysProperties As IEnumerable(Of PropertyInfoDisplay)
+    Private Shadows ReadOnly Property Owner() As frmSettings
+        Get
+            Return TryCast(MyBase.Owner, frmSettings)
+        End Get
+    End Property
+    Public Sub New()
+        InitializeComponent()
+        If ExifProperties Is Nothing Then ExifProperties = PropertyInfoDisplay.GetProperties(GetType(Tools.DrawingT.MetadataT.Exif.IFDExif))
+        If IPTCProperties Is Nothing Then IPTCProperties = PropertyInfoDisplay.GetProperties(GetType(Tools.DrawingT.MetadataT.IPTC))
+        If SysProperties Is Nothing Then SysProperties = PropertyInfoDisplay.GetProperties(GetType(SysInfo))
+        lstExif.Items.AddRange(ExifProperties.ToArray)
+        lstIPTC.Items.AddRange(IPTCProperties.ToArray)
+        lstOther.Items.AddRange(SysProperties.ToArray)
+    End Sub
+
+    Private Sub lst_Enter(ByVal sender As ListBox, ByVal e As System.EventArgs) _
+        Handles lstOther.Enter, lstIPTC.Enter, lstExif.Enter
+        DisplayInfo(sender)
+    End Sub
+
+    Private Sub DisplayInfo(ByVal sender As ListBox)
+        If sender.SelectedIndex >= 0 Then
+            txtInfo.Text = CType(sender.SelectedItem, PropertyInfoDisplay).Description
+            fraInfo.Text = String.Format("Category: {0}, Type: {1}", DirectCast(sender.SelectedItem, PropertyInfoDisplay).Category, DirectCast(sender.SelectedItem, PropertyInfoDisplay).Type.Name)
+        Else
+            fraInfo.Text = "Info"
+            txtInfo.Text = ""
+        End If
+    End Sub
+
+    Private Sub lst_SelectedIndexChanged(ByVal sender As ListBox, ByVal e As System.EventArgs) _
+        Handles lstOther.SelectedIndexChanged, lstIPTC.SelectedIndexChanged, lstExif.SelectedIndexChanged
+        If sender.Focused Then DisplayInfo(sender)
+    End Sub
+
+    Private Sub lst_MouseDoubleClick(ByVal sender As ListBox, ByVal e As System.Windows.Forms.MouseEventArgs) _
+        Handles lstOther.MouseDoubleClick, lstIPTC.MouseDoubleClick, lstExif.MouseDoubleClick
+        If Owner IsNot Nothing Then
+            Dim Namespace$
+            If sender Is lstExif Then : Namespace$ = "Exif"
+            ElseIf sender Is lstIPTC Then : [Namespace] = "IPTC"
+            Else : [Namespace] = "Sys"
+            End If
+            Owner.txtText.SelectedText = String.Format("<{0}:{1}:{2}>", [Namespace], CType(sender.SelectedItem, PropertyInfoDisplay).Name, "{0}")
+        End If
+    End Sub
+End Class
+
+Friend Class PropertyInfoDisplay
+    Public ReadOnly [Property] As PropertyInfo
+    Public Sub New(ByVal [Property] As PropertyInfo)
+        Me.Property = [Property]
+    End Sub
+    Public Overrides Function ToString() As String
+        Return DisplayName
+    End Function
+    Public ReadOnly Property DisplayName$()
+        Get
+            Dim a = [Property].GetAttribute(Of DisplayNameAttribute)(True)
+            Return If(a IsNot Nothing, a.DisplayName, Me.Property.Name)
+        End Get
+    End Property
+    Public ReadOnly Property Name$()
+        Get
+            Return Me.Property.Name
+        End Get
+    End Property
+    Public ReadOnly Property Type() As Type
+        Get
+            Return Me.Property.PropertyType
+        End Get
+    End Property
+    Public ReadOnly Property Description() As String
+        Get
+            Dim a = Me.Property.GetAttribute(Of DescriptionAttribute)(True)
+            Return If(a Is Nothing, "", a.Description)
+        End Get
+    End Property
+    Public ReadOnly Property Browsable() As Boolean
+        Get
+            Dim a = Me.Property.GetAttribute(Of BrowsableAttribute)(True)
+            Return a Is Nothing OrElse a.Browsable
+        End Get
+    End Property
+    Public ReadOnly Property Category$()
+        Get
+            Dim a = Me.Property.GetAttribute(Of CategoryAttribute)(True)
+            Return If(a Is Nothing, "Misc", a.Category)
+        End Get
+    End Property
+    Public Shared Function GetProperties(ByVal T As Type) As IEnumerable(Of PropertyInfoDisplay)
+        Return From pinfo In T.GetProperties _
+            Where pinfo.CanRead AndAlso Not pinfo.GetGetMethod.IsStatic AndAlso If(pinfo.GetGetMethod.GetParameters, New ParameterInfo() {}).Length = 0 _
+            Select it = New PropertyInfoDisplay(pinfo) _
+            Order By it.Category, it.DisplayName Ascending _
+            Where it.Browsable
+    End Function
+End Class
