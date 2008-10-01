67c6082551acd32ae997e09fad380a34d560d949
Dzonny_cp <SND\Dzonny_cp>
2008-10-01 08:00:11 +0800

    Strong naming

--- Tools IL/Make.cmd
+++ Tools IL/Make.cmd
@@ -1,2 +1,2 @@
-ilasm Type.il /OUTPUT="%1/Tools IL.dll" /DLL /NOLOGO /PDB /QUIET %2
+ilasm Type.il /OUTPUT="%1/Tools IL.dll" /DLL /NOLOGO /PDB /QUIET /KEY=..\SharedFiles\Tools.snk %2
 copy "Tools IL.xml" "%1/Tools IL.xml"
