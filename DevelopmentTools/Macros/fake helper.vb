1bb5e286786502be78fba193a51bad129a823026
Dzonny_cp <SND\Dzonny_cp>
2008-10-03 05:12:32 +0800

    Visual Studio Macros for editing project items metadata properties like <LogicalName> or <DependentUpon>

--- DevelopmentTools/Macros/fake helper.vb
+++ DevelopmentTools/Macros/fake helper.vb
@@ -1,4 +1,7 @@
-﻿<HideModuleName()> _
-Friend Module fake_helper
-    Public ReadOnly DTE As EnvDTE.DTE
+﻿''' <summary>Contains helpers necesary for this project to build outside Macros IDE</summary>
+<HideModuleName()> _
+Public Module fake_helper
+    ''' <summary>In case you are trying to use compiled assemby of this project outside Macros IDE, this firld must be set!</summary>
+    Public DTE As EnvDTE.DTE
 End Module
+
