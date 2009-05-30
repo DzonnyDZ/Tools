3f0a3c81715e7cb4aee788ff1c148e98c22c0d10
Dzonny_cp <SND\Dzonny_cp>
2009-05-31 06:40:51 +0800

    For Connect C++ bug

--- Tools IL/Make.cmd
+++ Tools IL/Make.cmd
@@ -1,3 +1,13 @@
-ilasm Type.il /OUTPUT="%1/Tools.IL.dll" /DLL /NOLOGO /PDB /QUIET /KEY=..\SharedFiles\Tools.snk %2 /INCLUDE=..\SharedFiles\
+@echo off
+echo BUILD:
+ilasm Type.il /OUTPUT="%1/Tools.IL.dll" /DLL /PDB /KEY=..\SharedFiles\Tools.snk %2 /INCLUDE=..\SharedFiles\
+IF NOT ERRORLEVEL 0 EXIT
+REM /QUIET /NOLOGO
+echo COPY OUTPUT:
 copy "Tools.IL.xml" "%1/Tools.IL.xml"
+IF NOT ERRORLEVEL 0 EXIT
+echo VERIFY:
 peverify /nologo /md /il "%1/Tools.IL.dll"
+IF NOT ERRORLEVEL 0 EXIT
+SET ERRORLEVEL = 0
+ECHO %ERRORLEVEL%
