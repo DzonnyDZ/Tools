3d620ded45b9710d2e37ebc5b8e910667fbfd29c
Dzonny_cp <SND\Dzonny_cp>
2009-11-24 08:14:59 +0800

    Changes in Tools.IL

--- Tools IL/Make.cmd
+++ Tools IL/Make.cmd
@@ -1,13 +1,14 @@
 @echo off
 echo BUILD:
-ilasm Type.il /OUTPUT="%1/Tools.IL.dll" /DLL /PDB /KEY=..\SharedFiles\Tools.snk %2 /INCLUDE=..\SharedFiles\
+echo ilasm Type.il /DLL /INCLUDE=..\SharedFiles /PDB /KEY=..\SharedFiles\Tools.snk %2 /OUTPUT="%1Tools.IL.dll"
+ilasm Type.il /DLL /INCLUDE=..\SharedFiles /PDB /KEY=..\SharedFiles\Tools.snk %2 /OUTPUT="%1Tools.IL.dll"
+echo Build finished with code %ERRORLEVEL%
 IF NOT ERRORLEVEL 0 EXIT
 REM /QUIET /NOLOGO
 echo COPY OUTPUT:
-copy "Tools.IL.xml" "%1/Tools.IL.xml"
+copy "Tools.IL.xml" "%1Tools.IL.xml"
 IF NOT ERRORLEVEL 0 EXIT
 echo VERIFY:
-peverify /nologo /md /il "%1/Tools.IL.dll"
+peverify /nologo /md /il "%1Tools.IL.dll"
 IF NOT ERRORLEVEL 0 EXIT
 SET ERRORLEVEL = 0
-ECHO %ERRORLEVEL%
