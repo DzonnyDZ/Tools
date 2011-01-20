72d68e66f8d4585074104d76ebec0e82cc8da5ec
Dzonny_cp <SND\Dzonny_cp>
2011-01-20 22:01:42 +0800

    Changes in C++/CLI tools for enums

--- Tools.Hardcore/EnumCore.cpp
+++ Tools.Hardcore/EnumCore.cpp
@@ -3,6 +3,7 @@
 
 using namespace System;
 using namespace Tools;
+using namespace System::Collections::Generic;
 
 EnumCore::EnumCore(){/*do nothing*/}
 
@@ -11,11 +12,19 @@ bool EnumCore::HasFlagSet(T value, T flag){
     return value->HasFlag(flag);
 }
 
-generic <class T> where T:Enum
+generic <class T> where T:Enum, gcnew()
 array<T>^ EnumCore::GetValues(){
     Array^ values = Enum::GetValues(T::typeid);
     array<T>^ ret = gcnew array<T>(values->Length);
     for(int i = 0; i < values->Length; i++)
         ret[i] = (T)values->GetValue(i);
     return ret;
+}
+
+generic <class TKey,class TValue> where TValue:Enum
+bool EnumCore::HasFlagSet(IDictionary<TKey, TValue>^ dic, TValue flag){
+    if(dic == nullptr) return false;
+    for each(KeyValuePair<TKey, TValue> item in dic)
+        if(item.Value->HasFlag(flag)) return true;
+    return false;
 }
--- Tools.Hardcore/EnumCore.h
+++ Tools.Hardcore/EnumCore.h
@@ -1,6 +1,7 @@
 #include "Stdafx.h"
 using namespace System;
 using namespace System::Runtime::CompilerServices;
+using namespace System::Collections::Generic;
 
 namespace Tools{
     /// <summary>Contains basic extension methods for working with enumerations</summary>
@@ -14,7 +15,7 @@ namespace Tools{
         /// <summary>Determines whether one or more bit fields are set in the current instance. (type-safe)</summary>
         /// <typeparam name="T">Type of enumeration</typeparam>
         /// <param name="value">An enumeration value to test flags on (this possibly is OR-ed value of multiple flags)</param>
-        /// <param name="flag">An enumeration value. - the flag to test</param>
+        /// <param name="flag">An enumeration value. - the flag(s) to test</param>
         /// <returns>true if the bit field or bit fields that are set in <paramref name="flag"/> are also set in <paramref name="value"/>; otherwise, false.</returns>
         /// <exception cref="ArgumentException"><typeparamref name="T"/> is <see cref="Enum"/> (not a derived type) and actual types of <paramref name="value"/> and <paramref name="flag"/> differ.</exception>
         /// <seelaso cref="Enum::HasFlag"/>
@@ -25,9 +26,19 @@ namespace Tools{
         /// <summary>Retrieves an array of the values of the constants in an enumeration.</summmary>
         /// <typeparamref name="T">Type of enumeration to retrieve values of</typeparam>
         /// <returns>An array that contains the values of the constants in <typeparamref name="T"/>. The elements of the array are sorted by the binary values of the enumeration constants.</returns>
-        /// <exception cref="ArgumentException"><typeparamref name="T"/> is not actual enum type (i.e. it is <see cref="Enum"/>)</exception>
-        generic <class T> where T:Enum
+        generic <class T> where T:Enum, gcnew()
         static array<T>^ GetValues(void);
+
+        /// <summary>Determines whether one or more bit fields are set in at least one of dictionary values.</summary>
+        /// <typeparamref name="TKey">Type of dictionary keys</typeparamref>
+        /// <typeparamref name="TValue">Enumeration type - type of values in dictionary</typeparamref>
+        /// <param name="dic">A dictionary to test values in</param>
+        /// <param name="flag">An enumeration value. - the flag(s) to test</param>
+        /// <returns>True if the bit field or bit fields that are set in <paramref name="flag"/> are also set in at least one value in <paramref name="dic"/>; false otherwise or if <paramref name="dic"/> is null.</param>
+        /// <exception cref="ArgumentException"><typeparamref name="TValue"/> is <see cref="Enum"/> (not a derived type) and actual types of <paramref name="flag"/> and value in <paramref name="dic"/> differ.</exception>
+        generic <class TKey, class TValue> where TValue:Enum
+        [Extension]
+        static bool HasFlagSet(IDictionary<TKey, TValue>^ dic, TValue flag);
 
         //TODO: TryParse
     };
