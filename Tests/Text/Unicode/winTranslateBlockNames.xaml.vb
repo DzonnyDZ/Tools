a82f909030e6f71d3db5f5ffe8239c8e1d15637e
Dzonny_cp <SND\Dzonny_cp>
2011-12-07 06:08:54 +0800

    UCD localization data - support tools

---
+++ Tests/Text/Unicode/winTranslateBlockNames.xaml.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Windows, System.Linq
+
+Namespace TextT.UnicodeT
+    Public Class winTranslateBlockNames
+        Inherits Window
+
+        Friend Sub New(data As IEnumerable(Of TranslateData))
+            InitializeComponent()
+            dgData.ItemsSource = data.ToList
+        End Sub
+
+        Private Sub btnDone_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles btnDone.Click
+            Me.DialogResult = True
+            Me.Close()
+        End Sub
+    End Class
+End Namespace
