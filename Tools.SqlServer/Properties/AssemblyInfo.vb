bbba9ead86d442530ac0e53bd81ad403e635804d
Dzonny_cp <SND\Dzonny_cp>
2012-05-13 02:04:12 +0800

    Changes for VS 2011

---
+++ Tools.SqlServer/Properties/AssemblyInfo.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Data.Sql
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Tools.SqlServer")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ĐTools")> 
+<Assembly: AssemblyCopyright("Copyright © Đonny 2009")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: CLSCompliant(True)> 
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("25ee137a-2eb1-4394-abcc-b924b807548c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+
+
+<Assembly: NeutralResourcesLanguageAttribute("en")> 
