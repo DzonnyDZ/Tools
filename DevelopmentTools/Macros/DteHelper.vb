5d35b99b54f63bfe980b48a73ae38da32ac0b5ea
Dzonny_cp <SND\Dzonny_cp>
2012-05-20 18:50:38 +0800

    Convert macros to addin for VS11

---
+++ DevelopmentTools/Macros/DteHelper.vb
@@ -0,0 +1,9 @@
+﻿''' <summary>Contains helpers necesary for this project to build and run outside Macros IDE</summary>
+''' <version version="1.5.4">Module renamed form <c>fake_helper</c> to <c>DteHelper</c></version>"
+<HideModuleName()> _
+Public Module DteHelper
+    ''' <summary>Provides <see cref="EnvDTE.DTE"/> instance to macro methods</summary>
+    ''' <version version="1.5.4">Added <see cref="CLSCompliantAttribute"/>(False)</version>
+    <CLSCompliant(False)>
+    Public DTE As EnvDTE.DTE
+End Module
--- DevelopmentTools/Macros/fake helper.vb
+++
@@ -1,6 +0,0 @@
-﻿''' <summary>Contains helpers necesary for this project to build outside Macros IDE</summary>
-<HideModuleName()> _
-Public Module fake_helper
-    ''' <summary>In case you are trying to use compiled assemby of this project outside Macros IDE, this firld must be set!</summary>
-    Public DTE As EnvDTE.DTE
-End Module
