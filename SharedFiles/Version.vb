418f9744a72fb63e3a450a3e86aa307fb8133cdf
Dzonny_cp <SND\Dzonny_cp>
2012-06-10 18:15:11 +0800

    <LogicalName> fixes for localized resources

--- SharedFiles/Version.vb
+++ SharedFiles/Version.vb
@@ -1,2 +1,2 @@
-﻿<Assembly: System.Reflection.AssemblyVersion("1.5.4.32039")>
-<Assembly: System.Reflection.AssemblyFileVersion("1.5.4.32039")>
+﻿<Assembly: System.Reflection.AssemblyVersion("1.5.4.32727")>
+<Assembly: System.Reflection.AssemblyFileVersion("1.5.4.32727")>
