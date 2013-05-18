b8847ebb85655b7caf8a1fdde883b332a3a96f5e
Dzonny_cp <SND\Dzonny_cp>
2013-05-19 03:56:27 +0800

    build fixes

--- SharedFiles/Version.cs
+++ SharedFiles/Version.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.5.4.37287")]
-[assembly: System.Reflection.AssemblyFileVersion("1.5.4.37287")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.5.4.37288")]
+[assembly: System.Reflection.AssemblyFileVersion("1.5.4.37288")]
--- SharedFiles/Version.vb
+++ SharedFiles/Version.vb
@@ -1,2 +1,2 @@
-﻿<Assembly: System.Reflection.AssemblyVersion("1.5.4.37287")>
-<Assembly: System.Reflection.AssemblyFileVersion("1.5.4.37287")>
+﻿<Assembly: System.Reflection.AssemblyVersion("1.5.4.37288")>
+<Assembly: System.Reflection.AssemblyFileVersion("1.5.4.37288")>
