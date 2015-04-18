5ea1cf83cab581db0cf68a0b3ddc41e9fba4e276
Dzonny_cp <SND\Dzonny_cp>
2015-04-18 14:48:30 +0800

    Metanol 2.1.0

--- Projects/MetanolCommonAssemblyInfo.vb
+++ Projects/MetanolCommonAssemblyInfo.vb
@@ -3,5 +3,5 @@ Imports System.Reflection
 <Assembly: AssemblyCompany("Ðonny")> 
 <Assembly: AssemblyProduct("Metanol")> 
 <Assembly: AssemblyCopyright("Copyright © Ðonny 2007÷2015")> 
-<Assembly: AssemblyVersion("2.0.8.0")> 
-<Assembly: AssemblyFileVersion("2.0.8.0")> 
+<Assembly: AssemblyVersion("2.1.0.0")> 
+<Assembly: AssemblyFileVersion("2.1.0.0")> 
--- SharedFiles/Version.vb
+++ SharedFiles/Version.vb
@@ -1,2 +1,2 @@
-﻿<Assembly: System.Reflection.AssemblyVersion("1.5.4.41668")>
-<Assembly: System.Reflection.AssemblyFileVersion("1.5.4.41668")>
+﻿<Assembly: System.Reflection.AssemblyVersion("1.5.4.41827")>
+<Assembly: System.Reflection.AssemblyFileVersion("1.5.4.41827")>
