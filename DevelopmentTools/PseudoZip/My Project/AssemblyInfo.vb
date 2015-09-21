210c65c1cd29de4c25062f1caa7e8fb661ec422e
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-09-21 11:49:00 +0200

    Tools NuGet

--- DevelopmentTools/PseudoZip/My Project/AssemblyInfo.vb
+++ DevelopmentTools/PseudoZip/My Project/AssemblyInfo.vb
@@ -1,37 +1,11 @@
-﻿Imports System.Resources
-
-Imports System
-Imports System.Reflection
+﻿Imports System.Reflection
+Imports System.Resources
 Imports System.Runtime.InteropServices
-
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("PseudoZip")> 
 <Assembly: AssemblyDescription("Packs/unpacks files using System.IO.Packaking.ZipPackage")> 
-<Assembly: AssemblyCompany("ĐTools team")> 
-<Assembly: AssemblyProduct("ĐTools")> 
-<Assembly: AssemblyCopyright("Copyright © Đonny 2009")> 
 <Assembly: AssemblyTrademark("")> 
-
 <Assembly: ComVisible(False)> 
 
-'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("8df83bc1-9a32-4be6-91be-22f2e2f696d4")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
--- SharedFiles/GlobalAssemblyInfo.vb
+++ SharedFiles/GlobalAssemblyInfo.vb
@@ -2,7 +2,7 @@ Imports System.Reflection
 
 <Assembly: AssemblyCompany("Ðonny")> 
 <Assembly: AssemblyProduct("ÐTools")> 
-<Assembly: AssemblyCopyright("Copyright © Jan Záruba (Ðonny) team 2006-15")> 
+<Assembly: AssemblyCopyright("Copyright © Jan Záruba (Ðonny) 2006-15")> 
 
 <Assembly: AssemblyVersion("1.5.4.50000")>
 <Assembly: AssemblyFileVersion("1.5.4.50000")>
