7ba5f91ff1b00605faf76897b35215d57d17a43a
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-09-22 15:28:11 +0200

    Tools.VisualStudio.Debugger NuGet

--- Tools.VisualStudio.Debugger/My Project/AssemblyInfo.vb
+++ Tools.VisualStudio.Debugger/My Project/AssemblyInfo.vb
@@ -1,27 +1,9 @@
-﻿Imports System.Resources
-
-Imports System
-Imports System.Reflection
+﻿Imports System.Reflection
+Imports System.Resources
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
-
 <Assembly: AssemblyTitle("Tools.VisualStudio.Debugger")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("Đonny")> 
-<Assembly: AssemblyProduct("ĐTools")> 
-<Assembly: AssemblyCopyright("Copyright © Đonny 2011")> 
-<Assembly: AssemblyTrademark("")> 
-
+<Assembly: AssemblyDescription("Classes related to Visual Studio debugging (this is AnyCPU assembly)")> 
 <Assembly: ComVisible(False)> 
-
-'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("e939e834-a23c-49e7-ab50-d39cb1b81b80")> 
-
-
-
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
