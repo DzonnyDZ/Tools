bc8fe3dd2c1b6605cc419e2567f69b0cf0150e8e
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-09-22 16:16:06 +0200

    Tools.VisualStudio NuGet

--- Tools Visual Studio/My Project/AssemblyInfo.vb
+++ Tools Visual Studio/My Project/AssemblyInfo.vb
@@ -3,36 +3,9 @@ Imports System.Resources
 Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
-
-<Assembly: AssemblyTitle("Tools Visual Studio")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("Đonny")> 
-<Assembly: AssemblyProduct("ĐTools")> 
-<Assembly: AssemblyCopyright("Copyright © Đonny  2007-11")> 
-<Assembly: AssemblyTrademark("")> 
-
-<Assembly: ComVisible(False)> 
-
-'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("b13b00c0-0c9f-4fe6-9cbd-f04230f3125d")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-
-<Assembly: NeutralResourcesLanguageAttribute("en")> 
-
-<Assembly: TypeForwardedTo(GetType(DiagnosticsT.ImageVisualizer))> 
+<Assembly: AssemblyTitle("Tools Visual Studio")>
+<Assembly: AssemblyDescription("Helpers for writing Visual Studio Extensions. This library is x86-only.")>
+<Assembly: ComVisible(False)>
+<Assembly: Guid("b13b00c0-0c9f-4fe6-9cbd-f04230f3125d")>
+<Assembly: NeutralResourcesLanguageAttribute("en")>
+<Assembly: TypeForwardedTo(GetType(DiagnosticsT.ImageVisualizer))>
