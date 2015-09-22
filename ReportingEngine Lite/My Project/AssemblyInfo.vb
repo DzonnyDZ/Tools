66857a52795bbe70771c8500c4fd79d0ad6a9f5d
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-09-22 14:32:13 +0200

    Reporting Engine Lite NuGet

--- ReportingEngine Lite/My Project/AssemblyInfo.vb
+++ ReportingEngine Lite/My Project/AssemblyInfo.vb
@@ -1,15 +1,10 @@
-﻿Imports System.Resources
-
-Imports System
-Imports System.Reflection
+﻿Imports System.Reflection
+Imports System.Resources
 Imports System.Runtime.InteropServices
 
 <Assembly: ComVisible(False)> 
 <Assembly: Guid("cc192617-3f2c-4d27-b8cc-352253c9291c")> 
 <Assembly: AssemblyTitle("Reporting Engine Lite")> 
 <Assembly: AssemblyDescription("A lightweight reporting engine")> 
-<Assembly: AssemblyCompany("Đonny")> 
-<Assembly: AssemblyProduct("ĐTools")> 
-<Assembly: AssemblyCopyright("Copyright © Đonny 2006/7/8/9/11")> 
 <Assembly: CLSCompliant(True)> 
-<Assembly: NeutralResourcesLanguageAttribute("en")> 
+<Assembly: NeutralResourcesLanguage("en")> 
