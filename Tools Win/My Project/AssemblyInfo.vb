6c01ef7054469ed685f7760fff4b3123a0f83547
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-09-22 13:01:28 +0200

    Tools.Win NuGet

--- Tools Win/My Project/AssemblyInfo.vb
+++ Tools Win/My Project/AssemblyInfo.vb
@@ -1,39 +1,10 @@
 ﻿Imports System.Resources
-
-Imports System
-Imports System.Reflection
+ imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
-
-<Assembly: AssemblyTitle("Tools Win")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("Đonny")> 
-<Assembly: AssemblyProduct("ĐTools")> 
-<Assembly: AssemblyCopyright("Copyright © Đonny team 2007-2010")> 
-<Assembly: AssemblyTrademark("")> 
-
+<Assembly: AssemblyTitle("Tools.Win")> 
+<Assembly: AssemblyDescription("Windows (desktop) -specific classes. Usually wrappers around Win32 API")> 
 <Assembly: ComVisible(False)> 
-
-'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("b4f21e90-48be-4ed6-ac00-00dc84a8fad8")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-
 <Assembly: CLSCompliant(True)> 
-
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
