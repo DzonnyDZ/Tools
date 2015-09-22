1e4591315f7dcf9fb24145d6d62d3f183495c165
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-09-22 16:16:49 +0200

    Tools.VisualStudio.CS NuGet

---
+++ SharedFiles/GlobalAssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Ðonny")]
+[assembly: AssemblyProduct("ÐTools")]
+[assembly: AssemblyCopyright("Copyright © Jan Záruba (Ðonny) 2006-15")]
+[assembly: AssemblyVersion("1.5.4.50001")]
+[assembly: AssemblyFileVersion("1.5.4.50001")]
