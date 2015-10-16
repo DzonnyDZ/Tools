3414e68ecb927894b620bf47bb7545f7338f4f57
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-10-16 10:58:02 +0200

    Total Commander Plugin Builder builds (itself)

--- Samples/wfx sample/My Project/AssemblyInfo.vb
+++ Samples/wfx sample/My Project/AssemblyInfo.vb
@@ -1,38 +1,10 @@
-﻿Imports System.Resources
-
-Imports System
-Imports System.Reflection
+﻿Imports System.Reflection
+Imports System.Resources
 Imports System.Runtime.InteropServices
-
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("WFX Sample")> 
 <Assembly: AssemblyDescription("Sample Total Commander file system plugin")> 
-<Assembly: AssemblyCompany("ĐTools team")> 
-<Assembly: AssemblyProduct("ĐTools")> 
-<Assembly: AssemblyCopyright("Copyright © Đonny 2009")> 
 <Assembly: AssemblyTrademark("")> 
-
 <Assembly: ComVisible(False)> 
-
-'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("9071e8cd-40ac-4cc9-bc5b-c1062d4a8acd")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-
-
-<Assembly: NeutralResourcesLanguageAttribute("en")> 
+<Assembly: NeutralResourcesLanguage("en")> 
--- Tools.TotalCommander/TotalCommander Plugin Builder/My Project/AssemblyInfo.vb
+++ Tools.TotalCommander/TotalCommander Plugin Builder/My Project/AssemblyInfo.vb
@@ -1,38 +1,9 @@
 ﻿Imports System.Resources
-
-Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
-
 <Assembly: AssemblyTitle("TotalCommander Plugin Builder")> 
-<Assembly: AssemblyDescription("Builds Total Commander plugin from assemby")> 
-<Assembly: AssemblyCompany("ĐTools Team")> 
-<Assembly: AssemblyProduct("ĐTools")> 
-<Assembly: AssemblyCopyright("Copyright © Đonny  2009")> 
-<Assembly: AssemblyTrademark("")> 
-
+<Assembly: AssemblyDescription("Builds Total Commander plugin from assembly")> 
 <Assembly: ComVisible(False)> 
-
-'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("4224801a-27c5-424d-af6f-54f82bf2b0e3")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-
-
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
